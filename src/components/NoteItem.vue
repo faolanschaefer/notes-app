83a78cd8c9b6de1c44fb8bd7bfa9152ef6ef0ddc
faolanschaefer <48422672+faolanschaefer@users.noreply.github.com>
2023-07-10 18:25:05 -0400

    created NoteItem component

---
+++ src/components/NoteItem.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="title-area">
+		<input type="text" id="note-title" />
+	</div>
+	<div class="body-area">
+		<input type="text" id="note-body" />
+	</div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+div {
+	margin: auto;
+	padding: 10px;
+	width: 75%;
+}
+.title-area {
+	height: 50px;
+}
+.body-area {
+	height: 500px;
+}
+div input[type="text"]{
+	width: 100%;
+	height: 100%;
+}
+</style>
